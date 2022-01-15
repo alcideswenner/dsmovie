84f91188c2d2afb39c6cefeb129282025371cda7
Alcides Wenner <alcideswenner.jus@gmail.com>
2022-01-15 15:42:54 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
-import Navbar from 'componentes/NavBar';
-import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "componentes/NavBar";
+
 function App() {
   return (
-    <Navbar/>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
